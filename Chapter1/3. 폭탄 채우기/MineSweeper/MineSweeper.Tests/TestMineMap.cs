02c5e0135de2cede0c2de64c860fc3cb0f6c479f
심승용 <seungyong.shim@mirero.co.kr>
2019-05-07 15:13:03 +0900

    - ch 1.3 폭탄 채우기 추가

---
+++ Chapter1/3. 폭탄 채우기/MineSweeper/MineSweeper.Tests/TestMineMap.cs
@@ -0,0 +1,166 @@
+﻿using FluentAssertions;
+using Xunit;
+
+namespace MineSweeper.Tests
+{
+    public class TestMineMap
+    {
+        [Fact]
+        public void Should_Generated_Bombs()
+        {
+            // Arrange
+            var actualMineMap = new MineMap(9, 9, 10);
+
+            // Act
+            actualMineMap.GeneratedBombs();
+
+            // Assert
+            int countBombs = 0;
+
+            for (int j = 0; j < actualMineMap.Height; j++)
+            {
+                for (int i = 0; i < actualMineMap.Width; i++)
+                {
+                    if (actualMineMap[j, i].IsBomb)
+                        countBombs++;
+                }
+            }
+
+            countBombs.Should().Be(10);
+        }
+
+        [Fact]
+        public void Should_Generated_All_Near_Bombs_Count()
+        {
+            // Arrange
+            var expectedMineItems = new MineItem[9, 9]
+            {
+                {
+                    new MineItem{IsBomb = false, NearBombsCount = 0},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 0},
+                    new MineItem{IsBomb = false, NearBombsCount = 0},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1}
+                },
+                {
+                    new MineItem{IsBomb = false, NearBombsCount = 0},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = true},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 0},
+                    new MineItem{IsBomb = false, NearBombsCount = 0},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = true},
+                    new MineItem{IsBomb = false, NearBombsCount = 1}
+                },
+                {
+                    new MineItem{IsBomb = false, NearBombsCount = 0},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = false, NearBombsCount = 2}
+                },
+                {
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 0},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = true},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = true},
+                    new MineItem{IsBomb = false, NearBombsCount = 1}
+                },
+                {
+                    new MineItem{IsBomb = true},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 3},
+                    new MineItem{IsBomb = true},
+                    new MineItem{IsBomb = false, NearBombsCount = 3},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = false, NearBombsCount = 1}
+                },
+                {
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = true},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = true},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 0}
+                },
+                {
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 2},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 0}
+                },
+                {
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = true},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = true},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 0},
+                    new MineItem{IsBomb = false, NearBombsCount = 0},
+                    new MineItem{IsBomb = false, NearBombsCount = 0}
+                },
+                {
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 1},
+                    new MineItem{IsBomb = false, NearBombsCount = 0},
+                    new MineItem{IsBomb = false, NearBombsCount = 0},
+                    new MineItem{IsBomb = false, NearBombsCount = 0}
+                },
+            };
+
+            var actualMineMap = new MineMap(9, 9);
+
+            actualMineMap[1, 2].IsBomb = true;
+            actualMineMap[1, 7].IsBomb = true;
+            actualMineMap[3, 4].IsBomb = true;
+            actualMineMap[3, 7].IsBomb = true;
+            actualMineMap[4, 0].IsBomb = true;
+            actualMineMap[4, 4].IsBomb = true;
+            actualMineMap[5, 2].IsBomb = true;
+            actualMineMap[5, 6].IsBomb = true;
+            actualMineMap[7, 1].IsBomb = true;
+            actualMineMap[7, 4].IsBomb = true;
+
+
+            // Act
+            actualMineMap.GenerateAllNearBombsCount();
+
+            // Assert
+            for (int j = 0; j < actualMineMap.Height; j++)
+            {
+                for (int i = 0; i < actualMineMap.Width; i++)
+                {
+                    actualMineMap[j, i].Value.Should().Be(expectedMineItems[j, i].Value, $"[{j}, {i}]");
+                }
+            }
+        }
+    }
+}
---
+++ Chapter1/3. 폭탄 채우기/MineSweeper/MineSweeper/MineMap.cs
@@ -0,0 +1,122 @@
+﻿using System;
+
+namespace MineSweeper
+{
+    public class MineMap
+    {
+        private readonly MineItem[,] _mineItems;
+
+        public int Width { get; }
+        public int Height { get; }
+        public int CountBombs { get; private set; }
+
+        public MineItem this[int y, int x] => _mineItems[y, x];
+
+        public void GeneratedBombs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public MineMap(int width, int height)
+        {
+            Width = width;
+            Height = height;
+
+            _mineItems = new MineItem[Height, Width];
+
+            for (int j = 0; j < Height; j++)
+            {
+                for (int i = 0; i < Width; i++)
+                {
+                    _mineItems[j, i] = new MineItem { };
+                }
+            }
+        }
+
+        public MineMap(int width, int height, int countBombs) : this(width, height)
+        {
+            CountBombs = countBombs;
+        }
+
+        public void GenerateAllNearBombsCount()
+        {
+            for (int j = 0; j < Height; j++)
+            {
+                for (int i = 0; i < Width; i++)
+                {
+                    if (_mineItems[j, i].IsBomb == true)
+                    {
+                        continue;
+                    }
+
+                    int nearBombCount = 0;
+
+                    if (j - 1 >= 0)
+                    {
+                        if (i - 1 >= 0)
+                        {
+                            if (_mineItems[j - 1, i - 1].IsBomb == true)
+                            {
+                                nearBombCount++;
+                            }
+                        }
+
+                        if (_mineItems[j - 1, i].IsBomb == true)
+                        {
+                            nearBombCount++;
+                        }
+
+                        if (i + 1 < Width)
+                        {
+                            if (_mineItems[j - 1, i + 1].IsBomb == true)
+                            {
+                                nearBombCount++;
+                            }
+                        }
+                    }
+                    if (i - 1 >= 0)
+                    {
+                        if (_mineItems[j, i - 1].IsBomb == true)
+                        {
+                            nearBombCount++;
+                        }
+                    }
+
+                    if (i + 1 < Width)
+                    {
+                        if (_mineItems[j, i + 1].IsBomb == true)
+                        {
+                            nearBombCount++;
+                        }
+                    }
+
+                    if (j + 1 < Height)
+                    {
+                        if (i - 1 >= 0)
+                        {
+                            if (_mineItems[j + 1, i - 1].IsBomb == true)
+                            {
+                                nearBombCount++;
+                            }
+                        }
+
+                        if (_mineItems[j + 1, i].IsBomb == true)
+                        {
+                            nearBombCount++;
+                        }
+
+                        if (i + 1 < Width)
+                        {
+                            if (_mineItems[j + 1, i + 1].IsBomb == true)
+                            {
+                                nearBombCount++;
+                            }
+                        }
+                    }
+
+                    _mineItems[j, i].NearBombsCount = nearBombCount;
+                }
+            }
+        }
+    }
+}
